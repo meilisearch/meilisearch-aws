7178b4ed79cb5a81acb4e0945c4217b84fd45061
alallema <amelie@meilisearch.com>
2021-10-12 23:00:02 +0200

    Change related to the next MeiliSearch release (v0.23.1)

--- tools/config.py
+++ tools/config.py
@@ -7,7 +7,7 @@ MEILI_CLOUD_SCRIPTS_VERSION_TAG = 'v0.23.1'
 
 # Update with the AMI id that you want to publish after TESTING
 
-PUBLISH_IMAGE_ID = 'ami-0718dfe1f62e7c2bb'
+PUBLISH_IMAGE_ID = 'ami-04d64f401811d1761'
 
 # Update with the AMI name that you want to unpublish/delete worldwide
 
