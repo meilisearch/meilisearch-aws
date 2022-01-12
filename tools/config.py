755c0fa55d4b93ca30c4cfa1ae90cd5db8426393
alallema <amelie@meilisearch.com>
2022-01-12 17:18:36 +0100

    Update publish and delete image

--- tools/config.py
+++ tools/config.py
@@ -7,11 +7,11 @@ MEILI_CLOUD_SCRIPTS_VERSION_TAG = 'v0.25.0'
 
 # Update with the AMI id that you want to publish after TESTING
 
-PUBLISH_IMAGE_ID = 'ami-090d4f51fad8aac21'
+PUBLISH_IMAGE_ID = 'ami-06bc537d7c0c3c53c'
 
 # Update with the AMI name that you want to unpublish/delete worldwide
 
-DELETE_IMAGE_NAME = 'MeiliSearch-v0.23.0-Debian-10.3'
+DELETE_IMAGE_NAME = 'MeiliSearch-v0.23.1-Debian-10.3'
 
 # Update with your own Securityt Group and Key Pair name / file
 
